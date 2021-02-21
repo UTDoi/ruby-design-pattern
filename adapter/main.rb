2b9ff378f772d0cd6e206f5ac9858c8bc44d6803
UTDoi <doisaltu1899@gmail.com>
2021-02-22 00:08:07 +0900

    add comments

--- adapter/main.rb
+++ adapter/main.rb
@@ -1,6 +1,7 @@
 require_relative 'delegation/banner_printer'
 require_relative 'inheritance/banner_printer'
 
+# 利用側はTargetインターフェースに依存することで、具象Adapterクラスひいては具象Adapteeクラスの入れ替えを用意にする
 printer = Delegation::BannerPrinter.new('Hello')
 printer.print_weak
 printer.print_strong
